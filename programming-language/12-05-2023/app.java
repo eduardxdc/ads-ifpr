8778a7e3829c968ee5badcd175e2acd9aab6ef91
Eduardo <141167453+eduardxdc@users.noreply.github.com>
2024-04-07 23:55:02 -0300

    initial commit

---
+++ programming-language/12-05-2023/app.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package desafios;
+
+/**
+ *
+ * @author aluno
+ */
+public class Desafio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int salario1, salario2, salario3, salario4, salario5, media;
+        
+        salario1 = 5000;
+        salario2 = 2000;
+        salario3 = 1500;
+        salario4 = 2200;
+        salario5 = 1000;
+        
+        media = (salario1 + salario2 + salario3 + salario4 + salario5)/5;
+        
+        System.out.println("A média salarial é:" + media);
+    }
+    
+}
