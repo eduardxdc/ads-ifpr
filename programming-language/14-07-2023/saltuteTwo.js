cdc692c9067526486574a02da06b04da5dd7fad7
eduardxdc <eduardxdc@proton.me>
2024-04-16 13:28:57 -0300

    exercises using the readline module

--- programming-language/14-07-2023/saltuteTwo.js
+++ programming-language/14-07-2023/saltuteTwo.js
@@ -1,8 +1,11 @@
 // #5 - create a program that prompts the user to enter their name and age, and displays a greeting message based on that data;
-
 const readline = require('readline-sync');
 
 const name = readline.question('qual o seu nome?: ');
 const age = readline.question('e qual a sua idade?: ');
 
-console.log(`Olá ${name}, tudo bem? então quer dizer que você tem ${age}? Muito massa!`);
+if(!isNaN(age) && !Number(name)){
+    console.log(`Olá ${name}, tudo bem? então quer dizer que você tem ${age} anos? Muito massa!`);
+} else {
+    console.log('seus dados estão incorretos!');
+}
