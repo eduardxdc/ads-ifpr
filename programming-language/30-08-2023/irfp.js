115d099820dac554dab7e18a0029d0c4a919fd55
eduardxdc <eduardxdc@proton.me>
2024-04-18 14:25:09 -0300

    selection structures

---
+++ programming-language/30-08-2023/irfp.js
@@ -0,0 +1,23 @@
+/* 
+Desenvolva um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são
+do imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e
+que o FGTS corresponde a 11% do salário bruto, mas não é descontado (é a empresa que deposita.) O
+salário líquido corresponde ao salário bruto menos os descontos O programa deverá pedir ao usuário o
+valor da sua hora e a quantidade de horas trabalhadas no mês.
+Desconto do IR:
+Salário Bruto até R$900,00 – Isento;
+Salário Bruto até R$ 1500, 00 – desconto de 5%;
+
+Salario bruto até R$ 2500,00 – desconto de 10%;
+Salário bruto acima de 2500 – desconto de 20%.
+
+Imprima na tela as informações, dispostas conforme o exemplo abaixo, no exemplo valor da hora é 5 e a
+quantidade de horas é 220.
+Salário bruto (5 * 220) : RS 1100,00
+( - ) IR (5%) : RS 55,00
+( - ) INSS ( 10% ) : RS 110,00
+FGTS ( 11%) : RS 121,00
+Total de descontos : RS 165,00
+Salário Líquido : RS 935,00
+
+*/
